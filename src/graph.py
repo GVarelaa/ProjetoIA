50990bfeeb969d1d0a40b9082e6b0fa6d3bbc961
Guilherme Varela <guilherme.varelaaa@gmail.com>
2022-11-26 11:54:31 +0000

    função de procura em profundidade adicionada

--- src/graph.py
+++ src/graph.py
@@ -39,6 +39,24 @@ class Graph:
 
         self.graph[node1].add((node2, weight))
 
+    def procura_DFS(self, start, end, path=[], visited=set()):
+        path.append(start)
+        visited.add(start)
+
+        if start == end:
+            # calcular o custo do caminho função calcula custo
+            custoT = self.calcula_custo(path)
+            return (path, custoT)
+
+        for (adjacente, peso) in self.m_graph[start]:
+            if adjacente not in visited:
+                resultado = self.procura_DFS(adjacente, end, path, visited)
+                if resultado is not None:
+                    return resultado
+
+        path.pop()  # se nao encontra, remover o que está no caminho
+        return None
+
 
 
 
